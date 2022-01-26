439253988b108b6ed81845cd05a6f169005d91b0
Sienna Wishart <39637495+siennawishart@users.noreply.github.com>
2022-01-26 12:44:57 -0500

    Connected the database and added data schemas for Reports, Relationships, and Projects collections (#3)
    * Connected the database and added data schemas
    
    * Corrected brackets
    
    * Fixed lint errors
    
    * Pull request corrections
    
    * Minor corrections

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,38 @@
 import express from 'express';
 
+require('dotenv').config();
+
 const app = express();
 const PORT = 8000;
+const mongoose = require('mongoose');
+const ReportModel = require('./models/Reports');
+const RelationshipModel = require('./models/Relationships');
+const ProjectModel = require('./models/Projects');
+
+// Convert body of JSON requests to an object
+app.use(express.json());
+
+mongoose.connect(process.env.DATABASE_CONNECTION_TOKEN);
 
 app.get('/', (req, res) => res.send('Express and TypeScript Server'));
+
+app.get('/reports', (req, res) => {
+  ReportModel.find({}, (err: any, result: any) => {
+    res.json(err ?? result);
+  });
+});
+
+app.get('/relationships', (req, res) => {
+  RelationshipModel.find({}, (err: any, result: any) => {
+    res.json(err ?? result);
+  });
+});
+
+app.get('/projects', (req, res) => {
+  ProjectModel.find({}, (err: any, result: any) => {
+    res.json(err ?? result);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at https://localhost:${PORT}`);
---
+++ backend/models/Reports.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+// GeoJSON schema based on: https://mongoosejs.com/docs/geojson.html
+const polygonSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ["Polygon"],
+    required: true,
+  },
+  coordinates: {
+    type: [[[Number]]],
+    required: true,
+  },
+});
+
+const reportSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+  },
+  description: {
+    type: String,
+  },
+  createdDate: {
+    type: Date,
+  },
+  lastUpdated: {
+    type: Date,
+  },
+  department: {
+    type: String,
+    enum: ["GV", "PRC", "UZD"],
+  },
+  methods: {
+    type: [String],
+    enum: ["TH", "FG", "WK", "QS", "OS"],
+  },
+  contacts: {
+    type: [String],
+  },
+  project: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "ProjectModel",
+  },
+  relationships: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "RelationshipModel",
+  },
+  location: polygonSchema,
+});
+
+const ReportModel = mongoose.model("reports", reportSchema);
+module.exports = ReportModel;
