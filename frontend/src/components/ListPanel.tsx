f5c41941e19d744093e4f398b5171d256783de8c
Mickey Dang <mickeydang7299@gmail.com>
2022-02-01 22:42:52 -0500

    added new list panel page and used flexbox for sizing

---
+++ frontend/src/components/ListPanel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ListPanel.css';
+
+function ListPanel() {
+  return (
+    <div className="list-container">
+      <p>This is the List Panel</p>
+    </div>
+  );
+}
+
+export default ListPanel;
