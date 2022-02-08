d567be1e28c5c5b1ad224d1ad82de49539ec9a7e
Mickey Dang <mickeydang7299@gmail.com>
2022-02-08 14:31:20 -0500

    small organization tweaks

--- backend/index.ts
+++ backend/index.ts
@@ -20,17 +20,15 @@ app.get('/', (req, res) => res.send('Express and TypeScript Server'));
 
 app.get('/alldata', async (req, res) => {
   const queryParams = req.query;
-
   const keywords = (<string>queryParams?.query)?.split(',') ?? [];
   const coordinates = (<string>queryParams?.box)?.split(',')?.map((x) => Number(x)) ?? [];
 
   // Expected behaviour is that if no filters are applied, no data is returned.
   if (keywords.length === 0 && coordinates.length === 0) {
-    res.status(200).json({
+    return res.status(200).json({
       reports: [],
       relationships: [],
     });
-    return;
   }
 
   const lowerLeft = coordinates.slice(0, 2);
@@ -42,25 +40,25 @@ app.get('/alldata', async (req, res) => {
   const reportFilters = [];
   const relationshipFilters = [];
 
-  const KEYWORDS_FILTER = { tags: { $all: keywords } };
-
   if (keywords.length > 0) {
+    const KEYWORDS_FILTER = { tags: { $all: keywords } };
+
     reportFilters.push(KEYWORDS_FILTER);
     relationshipFilters.push(KEYWORDS_FILTER);
   }
 
-  const BOUNDING_BOX_FILTER = {
-    location: {
-      $geoWithin: {
-        $geometry: {
-          type: 'Polygon',
-          coordinates: [polygon],
+  if (coordinates.length > 0) {
+    const BOUNDING_BOX_FILTER = {
+      location: {
+        $geoWithin: {
+          $geometry: {
+            type: 'Polygon',
+            coordinates: [polygon],
+          },
         },
       },
-    },
-  };
+    };
 
-  if (coordinates.length > 0) {
     reportFilters.push(BOUNDING_BOX_FILTER);
     relationshipFilters.push(BOUNDING_BOX_FILTER);
   }
@@ -68,29 +66,24 @@ app.get('/alldata', async (req, res) => {
   const REPORT_RESPONSE_FIELDS = {
     name: 1, relationships: 1, tags: 1, location: 1,
   };
-
-  const CREATION_DATE_SORT_ORDER = { creationDate: -1 };
+  const RELATIOSHIP_RESPONSE_FIELDS = {
+    name: 1, reports: 1, tags: 1, location: 1,
+  };
+  const MOST_RECENT_CREATED = { creationDate: -1 };
 
   const reportQuery = ReportModel.find({
     $and: reportFilters,
     REPORT_RESPONSE_FIELDS,
-  }).sort(CREATION_DATE_SORT_ORDER);
-
-  const RELATIOSHIP_RESPONSE_FIELDS = {
-    name: 1, reports: 1, tags: 1, location: 1,
-  };
+  }).sort(MOST_RECENT_CREATED);
 
   const relationshipQuery = RelationshipModel.find({
     $and: relationshipFilters,
     RELATIOSHIP_RESPONSE_FIELDS,
   });
 
-  const reports = await reportQuery;
-  const relationships = await relationshipQuery;
-
-  res.status(200).json({
-    reports,
-    relationships,
+  return res.status(200).json({
+    reports: await reportQuery,
+    relationships: await relationshipQuery,
   });
 });
 
