554f33d3dda5d38e53d4cd9a2fbdea6a1a70cac9
Sienna Wishart <sienna.wishart@gmail.com>
2022-02-13 12:09:10 -0500

    Revert "Implemented basic slider"
    This reverts commit 7872bd5fe80d7df5ddd2ff5d590ec49051569747.

--- frontend/src/components/ListPanel.tsx
+++ frontend/src/components/ListPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   Container,
   Row,
@@ -6,8 +6,7 @@ import {
   Accordion,
 } from 'react-bootstrap';
 import Autocomplete from '@mui/material/Autocomplete';
-import { Chip, TextField, Slider } from '@mui/material';
-import Typography from '@mui/material/Typography';
+import { Chip, TextField } from '@mui/material';
 import { RelationshipProperties, ReportProperties } from '../models/types';
 import './ListPanel.css';
 import RelationshipList from './RelationshipList';
@@ -20,18 +19,8 @@ interface ListPanelProps {
   onSearchChange: Function,
 }
 
-function valuetext(value: number) {
-  return `${value}°C`;
-}
-
 function ListPanel({ onSearchChange, reportResults, relationshipResults }: ListPanelProps) {
-  const [timeRangeValue, setTimeRangeValue] = useState<number[]>([20, 37]);
-
   const updateSearch = (_: any, values: string[]) => onSearchChange(values);
-
-  const handleChange = (event: Event, newValue: number | number[]) => {
-    setTimeRangeValue(newValue as number[]);
-  };
 
   return (
     <Container fluid className="list-container">
@@ -54,19 +43,6 @@ function ListPanel({ onSearchChange, reportResults, relationshipResults }: ListP
               <Chip variant="outlined" label={option} {...getTagProps({ index })} />
             ))}
             onChange={updateSearch}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Typography gutterBottom>Timeline</Typography>
-          <Slider
-            size="small"
-            getAriaLabel={() => 'Temperature range'}
-            value={timeRangeValue}
-            onChange={handleChange}
-            valueLabelDisplay="auto"
-            getAriaValueText={valuetext}
           />
         </Col>
       </Row>
