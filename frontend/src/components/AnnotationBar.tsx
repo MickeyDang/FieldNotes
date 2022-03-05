93c68c1138e265414b2874175f27b3da613d9c6e
Jenny Xia <jieni.xia@gmail.com>
2022-03-05 02:43:54 -0500

    unique button names for styling later

--- frontend/src/components/AnnotationBar.tsx
+++ frontend/src/components/AnnotationBar.tsx
@@ -14,10 +14,10 @@ function AnnotationBar({
 
   return (
     <div className="annotation-bar-container">
-      <button className="annotation-button" type="button" onClick={() => updateMode('point')}>Point</button>
-      <button className="annotation-button" type="button" onClick={() => updateMode('polygon')}>Polygon</button>
-      <button className="annotation-button" type="button" onClick={() => updateMode('text')}>Text</button>
-      <button className="annotation-button" type="button" onClick={() => updateMode('none')}>X</button>
+      <button className="annotation-button-point" type="button" onClick={() => updateMode('point')}>Point</button>
+      <button className="annotation-button-polygon" type="button" onClick={() => updateMode('polygon')}>Polygon</button>
+      <button className="annotation-button-text" type="button" onClick={() => updateMode('text')}>Text</button>
+      <button className="annotation-button-off" type="button" onClick={() => updateMode('none')}>X</button>
       <h3 className="annotation-label">Annotation Tools</h3>
     </div>
   );
