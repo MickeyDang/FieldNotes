8bd4d39188066c497e0ecb796332f6fbcfd321bd
Jenny Xia <jieni.xia@gmail.com>
2022-03-08 11:33:30 -0500

    fixed polygon mode

--- frontend/src/components/AnnotationBar.tsx
+++ frontend/src/components/AnnotationBar.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './AnnotationBar.css';
 
 interface AnnotationBarProps {
   onAnnotationModeSelect: Function,
   annotationMode: string,
+  isSearchMode: Boolean,
 }
 
 function AnnotationBar({
   onAnnotationModeSelect,
   annotationMode,
+  isSearchMode,
 }: AnnotationBarProps) {
   const updateMode = (updatedMode: string) => {
     onAnnotationModeSelect(updatedMode);
   };
-  const polygonButtonClass = annotationMode === 'polygon' ? 'annotation-button polygon active' : 'annotation-button polygon';
-  const textButtonClass = annotationMode === 'text' ? 'annotation-button text active' : 'annotation-button text';
+  const [polygonButtonClass, setPolygonButtonClass] = useState('annotation-button polygon');
+  const [textButtonClass, setTextButtonClass] = useState('annotation-button text');
+
+  useEffect(() => {
+    if (isSearchMode) {
+      updateMode('');
+    } else {
+      setPolygonButtonClass((annotationMode === 'polygon') ? 'annotation-button polygon active' : 'annotation-button polygon');
+      setTextButtonClass((annotationMode === 'text') ? 'annotation-button text active' : 'annotation-button text');
+    }
+  }, [isSearchMode, annotationMode]);
 
   return (
     <div className="annotation-bar-container">
